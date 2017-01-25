5aa151b6ebe6b650750255b00d7ac8a3238a78fa
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-25 18:04:17 -0500

    firebase folder

---
+++ firebase/firebase_config.js
@@ -0,0 +1,22 @@
+var firebase = require("firebase");
+
+require("firebase/auth");
+
+// console.log(firebase);
+
+ var config = {
+    apiKey: "AIzaSyBv8n5SQNo2WTWYpoZOXv7YEMPuQu9kpPQ",
+    authDomain: "bodhi-7ad02.firebaseapp.com",
+    databaseURL: "https://bodhi-7ad02.firebaseio.com",
+    storageBucket: "bodhi-7ad02.appspot.com",
+    messagingSenderId: "678478589847"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+module.exports = {
+    firebase : firebase,
+    database : database
+}
