6883334c7d7c63df72606d2ebfd71e9289981f6a
Chloe Rice <chloe.mi.rice@gmail.com>
2017-01-26 12:23:41 -0500

    mariana's turn

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -3,12 +3,8 @@ import { combineReducers } from 'redux'
 import requestReducer from './requestReducer'
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD
-  requests: requestReducer
-=======
-  auth: require('./auth').default,
+  requests: requestReducer,
   map: require('./map').default
->>>>>>> a44a9d1a7a164014c9cfe474a9563ba97879fb1a
 })
 
 export default rootReducer
