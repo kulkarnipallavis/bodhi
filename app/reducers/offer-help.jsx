b99faad60bc07309552145e56c403437f8bb6b05
kulkarnipallavis <kulkarnipallavis@gmail.com>
2017-01-26 18:02:38 -0500

    removed console.log

--- app/reducers/offer-help.jsx
+++ app/reducers/offer-help.jsx
@@ -3,7 +3,6 @@ import firebase from '../firebase.jsx'
 export const submitOffer = (date, msg) => {
 	return dispatch => {
 		var newOfferKey = firebase.database().ref().child('Offers').push().key;
-		console.log("newOfferKey:", newOfferKey);
 		var offerData = {
 			date : date,
 			messageToRequester : msg,
