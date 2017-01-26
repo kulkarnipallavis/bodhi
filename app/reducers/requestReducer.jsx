cae67ab36ee2353aba1b8b0c1fcfb4e506526db3
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-26 13:00:44 -0500

    added dispatch method for new request

--- app/reducers/requestReducer.jsx
+++ app/reducers/requestReducer.jsx
@@ -1,33 +1,36 @@
 'use strict'
 
-import { RECEIVE_REQUESTS } from './constants'
+//import { RECEIVE_REQUESTS } from './constants'
 import firebase from '../firebase.jsx'
 
 
-const requestReducer = (requests = [], action) => {
-  switch (action.type) {
-    case RECEIVE_REQUESTS: 
-    	return action.requests
+// const requestReducer = (requests = [], action) => {
+//   switch (action.type) {
+//     case RECEIVE_REQUESTS: 
+//     	return action.requests
 
-    default: return requests
-  }
-}
+//     default: return requests
+//   }
+// }
 
 
-export const receiveRequests = (requests) => ({
-  type: RECEIVE_REQUESTS,
-  requests
-})
-
-// export const addRequest = (request) => ({
-// 	type: ADD_REQUEST,
-
+// export const receiveRequests = (requests) => ({
+//   type: RECEIVE_REQUESTS,
+//   requests
 // })
 
+
 export const addRequest = (request) =>(
-	dispatch =>
-		firebase.database().ref 
+	dispatch =>{
+		var newRequestKey = database.ref().child('Requests').push().key;
+
+		var updates = {};
+		updates['/Users/' + uid + '/requests/requestId'] = newRequestKey;
+		updates['/Requests/' + newRequestKey] = request;
+
+		return database.ref().update(updates);
+	}
 
 
 )
-export default requestReducer
+//export default requestReducer
