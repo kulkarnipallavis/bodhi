d10ea822614930f56f9f09a49c6dc704dd3dfe50
Chloe Rice <chloe.mi.rice@gmail.com>
2017-01-26 10:51:32 -0500

    simplified form

--- app/components/Request.jsx
+++ app/components/Request.jsx
@@ -12,33 +12,42 @@ class Request extends Component {
 
     this.state = {
       title: '',
-      tags: [],
       description: '',
     }
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChangeTitle = this.handleChangeTitle.bind(this)
+    this.handleChangeDesc = this.handleChangeDesc.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange(tags) {
-
+  handleChangeTitle(event) {
+    this.setState({title: event.target.value})
   }
+
+  handleChangeDesc(event) {
+    this.setState({description: event.target.value})
+  }
+
+  handleSubmit
 
   render() {
     return (
       <div>
         <TextField
+          id="title"
           hintText="Title"
+          value={this.state.title}
+          onChange={this.handleChangeTitle}
           errorText="A request title is required."/>
-        <br />
+        <br/>
 
-        <br />
+        <br/>
         <TextField
+          id="description"
           hintText="Description"
+          value={this.state.description}
+          onChange={this.handleChangeDesc}
           errorText="Please describe the help you are requesting."/>
         <br />
-        <ChipInput
-          defaultValue={[]}
-          onChange={(tags) => this.handleChange(tags)}/>
     </div>
     )
   }
