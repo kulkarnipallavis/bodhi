1093c944c197091dc05a351711c91ff0f74c1527
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-26 12:43:27 -0500

    working on reducers for request

--- app/reducers/actions/requests.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import { RECEIVE_REQUESTS } from '../constants'
-
-export const receiveRequests = (requests) => ({
-  type: RECEIVE_REQUESTS,
-  requests
-})
--- app/reducers/requestReducer.jsx
+++ app/reducers/requestReducer.jsx
@@ -1,12 +1,33 @@
 'use strict'
 
 import { RECEIVE_REQUESTS } from './constants'
+import firebase from '../firebase.jsx'
+
 
 const requestReducer = (requests = [], action) => {
   switch (action.type) {
-    case RECEIVE_REQUESTS: return action.requests
+    case RECEIVE_REQUESTS: 
+    	return action.requests
+
     default: return requests
   }
 }
 
+
+export const receiveRequests = (requests) => ({
+  type: RECEIVE_REQUESTS,
+  requests
+})
+
+// export const addRequest = (request) => ({
+// 	type: ADD_REQUEST,
+
+// })
+
+export const addRequest = (request) =>(
+	dispatch =>
+		firebase.database().ref 
+
+
+)
 export default requestReducer
