6ac49416751147ef24adf955f8525a1d08cf8794
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-01-27 19:06:43 -0500

    figuring out how to query firebase from auth.jsx

--- app/reducers/auth.jsx
+++ app/reducers/auth.jsx
@@ -1,4 +1,4 @@
-
+import { database } from '../firebase'
 
 const LOGGED_IN = 'LOGGED_IN'
 const LOGGED_OUT = 'LOGGED_OUT'
@@ -10,16 +10,40 @@ const reducer = (state = null, action) => {
     default: return state
   }
 }
+
 export default reducer
 
-export const loggedIn = (user) => ({
-  type: LOGGED_IN,
-  user
-})
+export const loggedIn = (user) => {
+  return dispatch => {
+  	//return database.ref('/Users' + user.uid).once('value').then(function(snapshot){
+	return database
+		.ref('/Users/-KbMhCodpMcAMlVjBQG-')
+		.once('value', function(snapshot){
+  		console.log("snapshot", snapshot.val())
+  		// if(!snapshot){
+  		// 	return database().ref('Users' + user.uid).set({
+  		// 		uid: uid
+  		// 	})
+  		// } else {
+  		// 	console.log("User is in the database: snapshot", snapshot)
+  		// }
+  		// return
+  	})
+  	// .then((user) => ({
+  	// 	type: LOGGED_IN,
+  	// 	user
+ // 	})
+  }
+}
+
+
+
+// export const loggedIn = (user) => 
 
 export const loggedOut = () => ({
   type: LOGGED_OUT
 })
+
 
 
 
