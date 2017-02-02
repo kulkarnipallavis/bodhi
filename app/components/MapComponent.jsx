9da28d76b3c449f89b908cf6460d8816079ab7ec
Ya-wen Alice <yawenalice@gmail.com>
2017-02-02 16:00:42 -0500

    added names to markers

--- app/components/MapComponent.jsx
+++ app/components/MapComponent.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 
-const MapComponent = withGoogleMap(props => 
- props.center.lat ? 
+const MapComponent = withGoogleMap(props =>
+ props.center.lat ?
   (<GoogleMap
     ref={ props.onMapLoad }
     defaultZoom={14}
     defaultCenter={props.center}>
-    { props.markers && props.markers.map((marker, index) => 
-    	<Marker 
-    	 key={index} 
+    { props.markers && props.markers.map((marker, index) =>
+    	<Marker
+    	 key={index}
     	 position={marker.position}
     	 onClick={() => props.onMarkerClick(marker)}
     	 >
@@ -17,6 +17,10 @@ const MapComponent = withGoogleMap(props =>
     	     <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
     	       { <div>
 	    	       	 <h2>{marker.title}</h2>
+                     {
+                     //{marker.requester.picture}
+                    }
+                     <h3>from: {marker.requester.name}</h3>
 	    	         <h3 id="description">{marker.description}</h3>
 	    	         <button type="button" onClick={props.handleButtonClick}>Offer Help</button>
     	         </div>
@@ -26,7 +30,7 @@ const MapComponent = withGoogleMap(props =>
     	 </Marker>
     	)}
   </GoogleMap>)
-     : (<div></div>) 
+     : (<div></div>)
 
 )
 
