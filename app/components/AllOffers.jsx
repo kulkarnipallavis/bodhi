ba59db1a3f566fdbf51c092d4d11ed7b896c2125
Chloe Rice <chloe.mi.rice@gmail.com>
2017-02-04 18:52:56 -0500

    all offers view now mobile friendly

--- app/components/AllOffers.jsx
+++ app/components/AllOffers.jsx
@@ -1,70 +1,61 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router'
-import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
-  from 'material-ui/Table'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Table } from 'react-bootstrap'
 import RaisedButton from 'material-ui/RaisedButton'
+import Avatar from 'material-ui/Avatar'
 import smsLink from 'sms-link'
 
 class AllOffers extends Component {
-  constructor(props){
-    super(props)
 
-  }
-
-  render(){
+  render() {
     let offers = this.props.offersReceived
 
-    const styles = {
-      color: "white"
-    }
+    const styles = { color: "white" }
+    const allOffers = offers ? offers : []
 
-    const allOffers= offers ? offers : []
-
-      return (
-         <Table className="gradient">
-         <TableBody displayRowCheckbox={false}>
-        { allOffers && allOffers.map((offer, index) => (
-        <TableRow key={index} style={styles} selectable={false}>
-          <TableRowColumn>{offer.offUser.picture}</TableRowColumn>
-          <TableRowColumn>{offer.offUser.name}</TableRowColumn>
-          <TableRowColumn>{offer.message}</TableRowColumn>
-          <TableRowColumn>
-              <RaisedButton 
-              label="Yes, Please" 
-              primary={false} 
-              style={{margin:12}}
-              labelColor="#533BD7"
-              backgroundColor="white" 
-              onClick={()=>window.location=smsLink({phone:`${offer.offUser.phone}`, body:'Thank you. I accept your help.'})}/>
-              <RaisedButton 
-              label="No, Thanks" 
-              secondary={false} 
-              style={{margin:12}} 
-              labelColor="#533BD7"
-              backgroundColor="white" />
-          </TableRowColumn>
-          </TableRow>
-           ))}
-        </TableBody>
-        </Table>
-         
-        )
-   
+    return (
+      <div className="gradient flex-container">
+        <div className="flex-row">
+          <h1>Pending Help Offers</h1>
+        </div>
+        <div className="flex-row">
+          <Table responsive={true} bordered={true} style={styles}>
+            <tbody>
+             { allOffers && allOffers.map((offer, index) => (
+               <tr key={index}>
+                 <td><Avatar size={48} src={offer.offUser.picture}/></td>
+                 <td>{offer.offUser.name}</td>
+                 <td>{offer.message}</td>
+                 <td>
+                   <RaisedButton
+                   label="Accept"
+                   primary={false}
+                   style={{ margin: 12 }}
+                   labelColor="#533BD7"
+                   backgroundColor="white"
+                   onClick={() => window.location = smsLink({
+                     phone: `${offer.offUser.phone}`,
+                     body: 'Thank you. I accept your help.'}
+                   )}/>
+                 </td>
+                 <td>
+                   <RaisedButton
+                   label="Decline"
+                   secondary={false}
+                   style={{ margin: 12 }}
+                   labelColor="#533BD7"
+                   backgroundColor="white" />
+                  </td>
+                </tr>
+              ))  }
+           </tbody>
+         </Table>
+        </div>
+      </div>
+    )
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapStateToProps = state => ({ offersReceived: state.offersReceived })
 
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    offersReceived: state.offersReceived,
-  }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllOffers)
+export default connect(mapStateToProps)(AllOffers)
