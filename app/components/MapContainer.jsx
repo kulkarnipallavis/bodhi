1a407acc6122bc879a5059393bb6552134241c5f
Ya-wen Alice <yawenalice@gmail.com>
2017-02-05 11:56:05 -0500

    legend toggle

--- app/components/MapContainer.jsx
+++ app/components/MapContainer.jsx
@@ -12,13 +12,15 @@ class MapContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      markers: this.props.markers
+      markers: this.props.markers,
+      legendClick: false
     }
 
     this.handleMarkerClick = this.handleMarkerClick.bind(this)
     this.handleMarkerClose = this.handleMarkerClose.bind(this)
     this.handleMapLoad = this.handleMapLoad.bind(this)
     this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.legendToggle = this.legendToggle.bind(this)
   }
 
   componentDidMount() {
@@ -53,23 +55,47 @@ class MapContainer extends Component {
     browserHistory.push('/offerhelp');
   }
 
+  legendToggle() {
+    console.log('click')
+    this.setState({
+      legendClick: !this.state.legendClick
+    })
+  }
+
   render() {
 
-    const purpleMarker = <Avatar id="legend-icon-top" backgroundColor="none" size={32} src="/img/purple-marker.svg"/>
-    const redMarker = <Avatar id="legend-icon-bottom" backgroundColor="none" size={32} src="/img/red-marker.svg"/>
+    const purpleMarker = <Avatar id="legend-icon-bottom" backgroundColor="none" size={32} src="/img/purple-marker.svg"/>
+    const redMarker = <Avatar id="legend-icon-top" backgroundColor="none" size={32} src="/img/red-marker.svg"/>
+    const closeX = (!this.state.legendClick) ?
+      <Avatar id="close-x" backgroundColor="none" size={15} src="img/x.svg" alt="minimize"/>
+      : <Avatar id="expand" backgroundColor="none" size={15} src="img/expand.svg" alt="expand map legend"/>
 
     return (
       <div>
+        { (!this.state.legendClick) ?
+
         <div id="map-legend">
-          <div className="flex-row-legend">
-            {purpleMarker}
-            <p>Offers Pending</p>
-          </div>
           <div className="flex-row-legend">
             {redMarker}
             <p>Submitted</p>
+            <span id="close-x-span" onClick={this.legendToggle}>{closeX}</span>
+          </div>
+          <div className="flex-row-legend">
+            {purpleMarker}
+            <p id="descender-fix">Offers Pending</p>
           </div>
         </div>
+
+        : <div id="map-legend-min">
+            <div className="flex-row-legend">
+
+              <p id="legend-fix">Map Legend</p>
+              <span id="close-x-span" onClick={this.legendToggle}>{closeX}</span>
+            </div>
+
+          </div>
+        }
+
         <MapComponent
           containerElement={  <div style={{ height: '90vh', width: '100%' }} />  }
           mapElement={  <div style={{ height: '100%', width: '100%' }} />  }
