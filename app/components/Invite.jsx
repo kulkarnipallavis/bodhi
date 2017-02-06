1ae2b9629320d347a12c0fa4ce2ce7c9199896b4
annaebrown <annabrown4789@gmail.com>
2017-02-06 15:45:18 -0500

    changes

---
+++ app/components/Invite.jsx
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
+export default class InvitePage extends Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		<div className="profile gradient flex-container">
+			{user ?
+				<p>Invite Friends!</p>
+			}
+	    </div>
+	}
+}
+
+
