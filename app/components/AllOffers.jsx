b4726b0c032a0b097d26bc2fb5544f55690a0006
Ya-wen Alice <yawenalice@gmail.com>
2017-02-07 12:28:33 -0500

    too many json objects to check

--- app/components/AllOffers.jsx
+++ app/components/AllOffers.jsx
@@ -5,18 +5,21 @@ import RaisedButton from 'material-ui/RaisedButton'
 import Avatar from 'material-ui/Avatar'
 import smsLink from 'sms-link'
 import { respondToOffer } from '../reducers/offer-help'
-import { updateRequestStatus } from '../reducers/request-actions'
+import { findRequestByKey, updateRequestStatus } from '../reducers/request-actions'
+import { findOffers } from '../reducers/receive-help'
 
 
 class AllOffers extends Component {
 
   constructor(props) {
     super(props)
+
+    this.declineAndCheck = this.declineAndCheck.bind(this)
   }
 
   handleRespond = (newOfferStatus, offer) => (event) => {
     event.preventDefault()
-    console.log("newOfferStatus, offer ", newOfferStatus, offer)
+
     const textBody = newOfferStatus === 'declined' ?
     'I have already accepted another neighbor\'s help, but thank you for offering!'
     :
@@ -30,10 +33,37 @@ class AllOffers extends Component {
     this.props.respond(newOfferStatus, offer.offKey)
 
     if (newOfferStatus === 'accepted') {
-      console.log('offer.reqKey ', offer.reqKey)
       this.props.updateRequestStatus('closed', offer.reqKey)
+    } else {  //if declined
+        this.declineAndCheck(offer)
+        .then( this.props.updateRequestStatus('open', thisReqKey) )
+      }
     }
   }
+
+  declineAndCheck(offer) {
+    console.log("in declineAndCheck!")
+    let reqOfferQueue
+    //check if request is closed
+    let reqStaus = this.props.findRequestByKey(offer.reqKey).status
+    //update findOffers array on state
+    this.props.findOffersAgain(offer.reqUid)
+      .then((reqStatus)=> {
+        return this.props.offersReceived.filter((pendingOffer) => {
+          return pendingOffer.reqKey === offer.reqKey
+        })
+        .then((reqOfferQueue) => {
+          console.log("reqOfferQueue ", reqOfferQueue)
+          return (reqStatus !== 'closed' && reqOfferQueue.length === 0)
+
+          console.log("req status is not closed and there are no more offers")
+        })
+      })
+
+    }
+    //
+
+
 
   render() {
     let offers = this.props.offersReceived
@@ -49,31 +79,34 @@ class AllOffers extends Component {
         <div className="flex-row">
           <Table responsive={true} bordered={true} style={styles}>
             <tbody>
-             { allOffers && allOffers.map((offer, index) => (
-               <tr key={index}>
-                 <td><Avatar size={48} src={offer.offUser.picture}/></td>
-                 <td>{offer.offUser.name}</td>
-                 <td>{offer.message}</td>
-                 <td>
-                   <RaisedButton
-                   label="Accept"
-                   primary={false}
-                   style={{ margin: 12 }}
-                   labelColor="#533BD7"
-                   backgroundColor="white"
-                   onClick={this.handleRespond('accepted', offer)}/>
-                 </td>
-                 <td>
-                   <RaisedButton
-                   label="Decline"
-                   secondary={false}
-                   style={{ margin: 12 }}
-                   labelColor="#533BD7"
-                   backgroundColor="white"
-                   onClick={this.handleRespond('declined', offer)}/>
-                  </td>
-                </tr>
-              ))  }
+             { allOffers && allOffers.map((offer, index) => {
+
+                return (
+                 <tr key={index}>
+                   <td><Avatar size={48} src={offer.offUser.picture}/></td>
+                   <td>{offer.offUser.name}</td>
+                   <td>{offer.message}</td>
+                   <td>
+                     <RaisedButton
+                     label="Accept"
+                     primary={false}
+                     style={{ margin: 12 }}
+                     labelColor="#533BD7"
+                     backgroundColor="white"
+                     onClick={this.handleRespond('accepted', offer)}/>
+                   </td>
+                   <td>
+                     <RaisedButton
+                     label="Decline"
+                     secondary={false}
+                     style={{ margin: 12 }}
+                     labelColor="#533BD7"
+                     backgroundColor="white"
+                     onClick={this.handleRespond('declined', offer)}/>
+                    </td>
+                  </tr>
+                )
+              })  }
            </tbody>
          </Table>
         </div>
@@ -85,7 +118,9 @@ class AllOffers extends Component {
 const mapStateToProps = state => ({ offersReceived: state.offersReceived })
 const mapDispatchToProps = dispatch => ({
   respond: (status, offerKey) => dispatch(respondToOffer(status, offerKey)),
-  updateRequestStatus: (status, markerKey) => dispatch(updateRequestStatus(status, markerKey))
+  updateRequestStatus: (status, markerKey) => dispatch(updateRequestStatus(status, markerKey)),
+  findRequestByKey: (reqKey) => dispatch(findRequestByKey(reqKey)),
+  findOffersAgain: (reqUid) => dispatch(findOffers(reqUid))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllOffers)
