df7b2669d1e603e0ca4f6b63f5aac5ce852603ea
Chloe Rice <chloe.mi.rice@gmail.com>
2017-02-08 14:48:34 -0500

    fixed conflict

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React, { Component} from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { setSelectedMarker } from '../reducers/map'
-=======
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router'
-import {getOpenRequests, getAcceptedOffers} from '../reducers/home'
-import {setSelectedMarker, updateMarkers} from '../reducers/map'
->>>>>>> 92c587884e616cd7b97c6f7d444b2252931ff73c
 import Avatar from 'material-ui/Avatar'
 import Divider from 'material-ui/Divider'
 import { Grid, Row, Col } from 'react-bootstrap'
