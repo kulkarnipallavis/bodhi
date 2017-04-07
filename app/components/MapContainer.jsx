e15d43892e9c6d3f86694bcd341429a8a61e3eee
Chloe Rice <chloe.mi.rice@gmail.com>
2017-04-06 23:33:43 -0400

    marker info windows back in business

--- app/components/MapContainer.jsx
+++ app/components/MapContainer.jsx
@@ -27,8 +27,18 @@ export default connect(mapStateToProps, mapDispatchToProps)(
     constructor(props) {
       super(props)
       this.state = {
-        legendClick: true
+        legendClick: true,
+        filterMarkers: false,
+        publicMarkers: [],
+        networkMarkers: []
       }
+    }
+
+    componentWillReceiveProps(newProps, oldProps) {
+      this.setState({
+        publicMarkers: newProps.publicMarkers,
+        networkMarkers: newProps.networkMarkers
+      })
     }
 
     handleMapLoad = (map) => {
@@ -37,22 +47,24 @@ export default connect(mapStateToProps, mapDispatchToProps)(
     }
 
     handleMarkerClick = (targetMarker) => {
-      this.setState({
-           markers: this.props.markers.map(marker => {
-            if (marker === targetMarker) marker.showDesc = true
-            else marker.showDesc = false
-        })
+      const markers = this.state.markers
+        ? [...this.state.markers] : [...this.state.publicMarkers]
+      markers.map(marker => {
+        if (marker.key === targetMarker.key) marker.showDesc = true
+        else marker.showDesc = false
+        return marker
       })
+      this.setState({markers})
       this.props.handleSelectMarker(targetMarker)
     }
 
     handleMarkerClose = (targetMarker) => {
-     this.setState({
-       markers: this.props.markers.map(marker => {
-         if (marker === targetMarker) marker.showDesc = false
-         return marker;
-       })
-     })
+      const markers = [...this.state.markers]
+      markers.map(marker => {
+        marker.showDesc = false
+        return marker
+      })
+      this.setState({markers})
     }
 
     handleInfoButtonClick = () => browserHistory.push('/offerhelp')
@@ -60,15 +72,29 @@ export default connect(mapStateToProps, mapDispatchToProps)(
     legendToggle = () => this.setState({legendClick: !this.state.legendClick})
 
     toggleMarkers = markerType => event => {
-      this.setState({markers: this.props[`${markerType}Markers`]})
+      const filterMarkers = markerType === 'network'
+      const markers = [...this.state[`${markerType}Markers`]]
+      this.setState({markers, filterMarkers})
     }
 
     render() {
       const purpleMarker = <Avatar id="legend-icon-bottom" backgroundColor="none" size={32} src="/img/purple-marker.svg"/>
       const redMarker = <Avatar id="legend-icon-top" backgroundColor="none" size={32} src="/img/red-marker.svg"/>
       const closeX = !this.state.legendClick
-        ? <Avatar id="close-x" backgroundColor="none" size={15} src="img/x.svg" alt="minimize"/>
-        : <Avatar id="expand" backgroundColor="none" size={15} src="img/expand.svg" alt="expand map legend"/>
+        ? <Avatar
+            id="close-x"
+            backgroundColor="none"
+            size={15}
+            src="img/x.svg"
+            alt="minimize"
+          />
+        : <Avatar
+            id="expand"
+            backgroundColor="none"
+            size={15}
+            src="img/expand.svg"
+            alt="expand map legend"
+          />
 
       return (
         <div>
@@ -107,7 +133,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
               containerElement={<div style={{ height: '90vh', width: '100%' }} />}
               mapElement={<div style={{ height: '100%', width: '100%' }} />}
               onMapLoad={this.handleMapLoad}
-              markers={this.state.markers ? this.state.markers : this.props.publicMarkers}
+              markers={this.state.markers ? this.state.markers : this.state.publicMarkers}
               center={this.props.center}
               onMarkerClick={this.handleMarkerClick}
               onMarkerClose={this.handleMarkerClose}
